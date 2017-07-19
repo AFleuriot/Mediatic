c9cc61662cf70c7efb54efef255eddd970dcbc45
Aurélien <fdrstari@gmail.com>
2017-07-19 15:41:50 +0200

    EmpruntDAO fait

--- src/main/java/media/modele/TypeMedia.java
+++ src/main/java/media/modele/TypeMedia.java
@@ -2,14 +2,16 @@ package media.modele;
 
 public enum TypeMedia {
 	
-	Livre{
-		public long getJoursEmpruntables() {
-			return 30L;
-		}
-	}, CD, DVD;
+	Livre(30), CD(15), DVD(15);
+	
+	private long joursEmpruntables;
+	
+	private TypeMedia(long joursEmpruntables) {
+		this.joursEmpruntables = joursEmpruntables;
+	}
 
 	public long getJoursEmpruntables() {
-		return 15L;
+		return joursEmpruntables;
 	}
 	
 }
