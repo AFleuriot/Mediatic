e4c83c1bbe80a01601c006cfd9652e5279db107b
Alexandre <alexandre.dezier@orange.fr>
2017-07-19 16:27:28 +0200

    maj adherentdao

--- src/main/java/adherent/dao/AdherentDAO.java
+++ src/main/java/adherent/dao/AdherentDAO.java
@@ -5,17 +5,27 @@ import java.util.List;
 
 import javax.persistence.*;
 
+import org.hibernate.boot.model.relational.Database;
+
 import adherent.modele.Adherent;
+import static databasehelper.DatabaseHelper.*;
 
 public class AdherentDAO {
 		public static EntityManager em = null;
 		
 		public static void creerAdherent(Adherent adherent){
+			EntityManager em = createEntityManager();
+			beginTx(em);			
 			em.persist(adherent);
+			commitTxAndClose(em);
+			
 		}
 		
 		public static void majAdherent(Adherent adherent){
+			EntityManager em = createEntityManager();
+			beginTx(em);
 			em.merge(adherent);
+			commitTxAndClose(em);
 		}			
 		
 		public static List<Adherent> rechercheAdherents(){
@@ -36,14 +46,17 @@ public class AdherentDAO {
 					req+="where nom like :nom";
 				}
 			}
-			
+			EntityManager em = createEntityManager();
+			beginTx(em);
 			TypedQuery<Adherent> query = em.createQuery(req, Adherent.class);
 			if(id != null){
 				query.setParameter("id", id);	
 			}
 			if(nom != null){
 				query.setParameter("nom", "%"+nom+"%");	
-			}			
-			return query.getResultList();
+			}		
+			List<Adherent> resultat = query.getResultList();
+			commitTxAndClose(em);			
+			return resultat;
 		}
 }
