d4195f6e0aca0d354d04b6ab062bf8bc661c3122
David <ds-david@hotmail.fr>
2017-07-19 14:29:16 +0200

    Ajout DatabaseHelper et début Media DAO

--- src/main/java/media/dao/MediaDAO.java
+++ src/main/java/media/dao/MediaDAO.java
@@ -1,5 +1,28 @@
 package media.dao;
 
-public class MediaDAO {
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import media.modele.Media;
 
+import static databasehelper.DatabaseHelper.*;
+
+import java.util.List;
+
+public class MediaDAO {
+	
+	
+	
+	
+	public static List<Media> rechercheMediaParTitre(String titreRecherche){
+		EntityManager em = createEntityManager();
+		beginTx(em);
+		TypedQuery<Media> rech = em.createQuery("SELECT m FROM media m AS med WHERE med.titre LIKE '%titreRecherche'", Media.class);
+		commitTxAndClose(em);
+		return rech.getResultList();
+	}
+	
+	
+	// FROM media WHERE media.titre LIKE '%recherche%' 
+	
+	
 }
