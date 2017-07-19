17070ad42af7c421321867cef3d4e0543f3b9b29
Suzanne <suzanne.thomasxii@gmail.com>
2017-07-19 17:49:53 +0200

    Modif cotisationDAO

--- src/main/java/cotisation/dao/CotisationDAO.java
+++ src/main/java/cotisation/dao/CotisationDAO.java
@@ -1,5 +1,26 @@
 package cotisation.dao;
 
+import static databasehelper.DatabaseHelper.beginTx;
+import static databasehelper.DatabaseHelper.commitTxAndClose;
+import static databasehelper.DatabaseHelper.createEntityManager;
+
+import javax.persistence.EntityManager;
+
+import cotisation.modele.Cotisation;
+
 public class CotisationDAO {
 
+	public static void creerCotisation(Cotisation cotisation){
+		EntityManager em = createEntityManager();
+		beginTx(em);			
+		em.persist(cotisation);
+		commitTxAndClose(em);	
+	}
+	
+	public static void majCotisation(Cotisation cotisation){
+		EntityManager em = createEntityManager();
+		beginTx(em);
+		em.merge(cotisation);			
+		commitTxAndClose(em);
+	}			
 }
