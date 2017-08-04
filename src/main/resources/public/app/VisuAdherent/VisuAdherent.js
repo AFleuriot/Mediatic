df0c613bcba4d24771afc9db9502eb34d4781fa2
Alexandre <alexandre.dezier@orange.fr>
2017-08-04 15:24:02 +0200

    mise en place des controllers pour les visu (à améliorer)

--- src/main/resources/public/app/VisuAdherent/VisuAdherent.js
+++ src/main/resources/public/app/VisuAdherent/VisuAdherent.js
@@ -2,7 +2,41 @@
 
 angular
     .module('mediatic.VisuAdherent',['ngRoute'])
-    .controller('VisuAdherentCtrl', ['$scope', function(){
-        
+    .controller('VisuAdherentCtrl', ['$scope', '$location', function($scope,$location){
+        $scope.adherent={};
+        $scope.adherent.nom = 'DEZIER';
+        $scope.adherent.prenom = 'Alexandre';
+        $scope.adherent.ddn = new Date('1994','05','02');
+        $scope.adherent.mail = 'alex@or.fr';
+        $scope.adherent.cp = 14123;
+        $scope.adherent.ville = 'Fleury';
+        $scope.adherent.rue = 'Chemin des roulettes russes';
+        $scope.adherent.dateCotisation = new Date('2015','05','30');
+        $scope.adherent.cotisation = 3.5000;
+
+        $scope.getAge = function getAge() 
+        {
+            var today = new Date();
+            var birthDate = $scope.adherent.ddn;
+            var age = today.getFullYear() - birthDate.getFullYear();
+            var m = today.getMonth() - birthDate.getMonth();
+            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+            {
+                age--;
+            }
+            return age + " ans";
+        }
+
+        $scope.$watch('adherent.dateCotisation',function() {
+            var date = new Date($scope.adherent.dateCotisation.getFullYear(), $scope.adherent.dateCotisation.getMonth(), $scope.adherent.dateCotisation.getDate());
+            date.setYear(date.getFullYear()+1);
+            $scope.finCotisation = date.toLocaleDateString();   
+        })
+
+        $scope.submit = function(){ 
+            $("#myModalun").modal('hide');
+            $location.path('/rechercheAdherent');
+        }
+
     }]
 );    
