3880dc7f25dc8352b7c68cf2b70ef04adcbf7be8
Suzanne <suzanne.thomasxii@gmail.com>
2017-08-04 15:03:59 +0200

    Ajout ng messages et modif accueil

--- src/main/resources/public/app/accueil/accueil.js
+++ src/main/resources/public/app/accueil/accueil.js
@@ -2,7 +2,11 @@
 
 angular
     .module('mediatic.accueil',['ngRoute'])
-    .controller('accueilCtrl', ['$scope', function(){
-        
+    .controller('accueilCtrl', ['$scope','$location', function($scope, $location){
+
+        $scope.submit = function() {
+            console.log('submitted');
+            $location.path('/rechercheMedia');          
+        }
     }]
 );    
