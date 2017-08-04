fc81d359d588f35056ab3ca325f22a9cc5c727c1
Alexandre <alexandre.dezier@orange.fr>
2017-08-04 17:45:54 +0200

    ajout commentaire pour la fonction

--- src/main/resources/public/app/RechercheAdherent/RechercheAdherent.js
+++ src/main/resources/public/app/RechercheAdherent/RechercheAdherent.js
@@ -21,6 +21,9 @@ angular.module('mediatic.RechercheAdherent', ['ngRoute'])
     }
 
     $scope.returnDate = function(dte){
+        //1. on convertit la chaine en Date : new Date(dte)
+        //2. on convertit en format dd/MM/yyyy : toLocaleDateString()
+        //3. on vérifie si la date est valide sinon on retourne une chaine vide
         var temp = new Date(dte).toLocaleDateString();
         return temp!='Invalid Date' ? temp : '' ;
     }
