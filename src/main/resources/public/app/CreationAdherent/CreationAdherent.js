9f7a7c06c8ba5c0722a8001914fbb2ca66a4c0b4
David <ds-david@hotmail.fr>
2017-08-04 10:39:05 +0200

    DossiersCreationMediaAdh

---
+++ src/main/resources/public/app/CreationAdherent/CreationAdherent.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module(myApp.CreationAdherent, ['ngRoute'])
+    .controller('CreationAdherentCtrl', [function(){
+
+
+
+
+
+
+    }]);
---
+++ src/main/resources/public/app/CreationMedia/CreationMedia.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module(myApp.creationmedia, ['ngRoute'])
+    .controller('CreationMediaCtrl', [function(){
+
+
+
+
+
+        
+    }]);
