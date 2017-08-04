fa97a6c41d89ef92c02e47176e182f33dea99290
David <ds-david@hotmail.fr>
2017-08-04 15:38:07 +0200

    Creation update

--- src/main/resources/public/app/CreationAdherent/CreationAdherent.js
+++ src/main/resources/public/app/CreationAdherent/CreationAdherent.js
@@ -1,7 +1,37 @@
 'use strict';
 
 angular.module('mediatic.CreationAdherent', ['ngRoute'])
-    .controller('CreationAdherentCtrl', [function(){
+    .controller('CreationAdherentCtrl', ['$scope', '$timeout',
+    function($scope,$timeout){
+
+        $scope.AdherentsCrees = [];
+        $scope.MessageAdhCree = '';
+          $scope.CreationAdherent = function(){
+
+        $scope.$watch('adh.DateFinCoti', function(){
+            var plusAn = $scope.adh.DateFinCoti.getFullYear + 1;
+            $scope.adh.DateFinCoti += $scope.adh.DateFinCoti.setFullYear(plusAn);
+        }, true);
+         
+
+        $scope.AdherentsCrees.push(
+                {'nom': $scope.adh.Nom,
+                 'prenom': $scope.adh.Prenom,
+                 'dateNaissance':$scope.adh.DateNaiss,
+                 'age': $scope.adh.Age,
+                 'email': $scope.adh.Mail,
+                 'adresse': $scope.adh.Adresse + ' ' + $scope.adh.CP +' '+ $scope.adh.Ville,
+                 'dateCotisation': $scope.adh.DateCoti,
+                 'montantCotisation': $scope.adh.MontantCoti,
+                 'dateFinCotisation': $scope.adh.DateFinCoti,
+                });
+            $scope.MessageAdhCree = 'L\'adhérent a bien été enregistré !';
+            $timeout(function(){
+                $scope.MessageAdhCree ='';
+            }, 5000);
+        }
+
+
 
  
     }]);
--- src/main/resources/public/app/CreationMedia/CreationMedia.js
+++ src/main/resources/public/app/CreationMedia/CreationMedia.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('mediatic.CreationMedia', ['ngRoute'])
-    .controller('CreationMediaCtrl', [function(){
+    .controller('CreationMediaCtrl', ['$scope','$timeout',
+        function($scope,$timeout){
+            $scope.MediaCrees = [];
+            $scope.MessageMediaCree = '';
+
+            $scope.CreationMedia = function(){
+                $scope.MediaCrees.push(
+                    {'titre' : $scope.media.titre,
+                     'auteur': $scope.media.auteur,
+                     'type' : $scope.media.type                   
+                    });
+                $scope.MessageMediaCree = 'Le nouveau média a été enregistré !'
+                $timeout(function(){
+                $scope.MessageMediaCree ='';
+                }, 5000);
+            }
 
     }]);
